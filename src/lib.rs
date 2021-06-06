de39768132e202f6e43c976bae63fa0384cb9d9c
Naja Melan <najamelan@autistici.org>
2021-06-06 11:51:11 +0200

    Fix: removal of external_doc feature.
    https://github.com/rust-lang/stdarch/pull/1013

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![ cfg_attr( nightly, feature( external_doc, doc_cfg    ) ) ]
-#![ cfg_attr( nightly, doc    ( include = "../README.md" ) ) ]
+#![ cfg_attr( nightly, feature( doc_cfg )                 ) ]
+#![ cfg_attr( nightly, doc = include_str!("../README.md") ) ]
 #![ doc = "" ] // empty doc line to handle missing doc warning when the feature is missing.
 
 #![ doc   ( html_root_url = "https://docs.rs/async_executors" ) ]
