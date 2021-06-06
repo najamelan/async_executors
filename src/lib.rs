11d783d5f022dd0746002c93d90da78da4b44352
Naja Melan <najamelan@autistici.org>
2021-06-06 12:39:42 +0200

    Fix doc link until 1.54 stabilizes...
    Yep, don't ask me, that seems to be the way to do it:
    https://github.com/rust-lang/rust/issues/82768

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![ cfg_attr( nightly, feature( doc_cfg )                 ) ]
-#![ cfg_attr( nightly, doc = include_str!("../README.md") ) ]
+#![ cfg_attr( nightly, feature(doc_cfg) ) ]
+#![ cfg_attr( nightly, cfg_attr( nightly, doc = include_str!("../README.md") )) ]
 #![ doc = "" ] // empty doc line to handle missing doc warning when the feature is missing.
 
 #![ doc   ( html_root_url = "https://docs.rs/async_executors" ) ]
