29c2c2ba42d4352b4582a50583afcd70f99b0f7a
Naja Melan <najamelan@autistici.org>
2022-01-06 14:31:46 +0100

    Add forward for SpawnBlocking to tracing types.

--- src/exec/tracing.rs
+++ src/exec/tracing.rs
@@ -2,10 +2,10 @@
 //
 use
 {
-	futures_util    :: { future::{ FutureExt }                                               } ,
-	futures_task    :: { SpawnError, LocalFutureObj, FutureObj                               } ,
-	crate           :: { JoinHandle, SpawnHandle, LocalSpawnHandle, Timer, TokioIo, YieldNow } ,
-	tracing_futures :: { Instrument, Instrumented, WithDispatch                              } ,
+	futures_util    :: { future::{ FutureExt }                  } ,
+	futures_task    :: { SpawnError, LocalFutureObj, FutureObj  } ,
+	crate::iface    :: { *                                      } ,
+	tracing_futures :: { Instrument, Instrumented, WithDispatch } ,
 };
 
 
@@ -82,3 +82,46 @@ impl<T> TokioIo for WithDispatch<T> where T: TokioIo {}
 
 impl<T> YieldNow for Instrumented<T> where T: YieldNow {}
 impl<T> YieldNow for WithDispatch<T> where T: YieldNow {}
+
+
+
+impl<T, R> SpawnBlocking<R> for Instrumented<T> where T: SpawnBlocking<R>, R: Send + 'static
+{
+	fn spawn_blocking<F>( &self, f: F ) -> BlockingHandle<R>
+
+		where F   : FnOnce() -> R + Send + 'static ,
+	         Self: Sized                          ,
+	{
+		self.inner().spawn_blocking(f)
+	}
+
+	/// Runs the provided closure on a thread where blocking is acceptable. This part of the trait is
+	/// object safe but your closure must be boxed and you cannot have a return value.
+	//
+	fn spawn_blocking_dyn( &self, f: Box< dyn FnOnce()->R + Send > ) -> BlockingHandle<R>
+	{
+		self.inner().spawn_blocking_dyn(f)
+	}
+}
+
+
+impl<T, R> SpawnBlocking<R> for WithDispatch<T> where T: SpawnBlocking<R>, R: Send + 'static
+{
+	fn spawn_blocking<F>( &self, f: F ) -> BlockingHandle<R>
+
+		where F   : FnOnce() -> R + Send + 'static ,
+	         Self: Sized                          ,
+	{
+		self.inner().spawn_blocking(f)
+	}
+
+	/// Runs the provided closure on a thread where blocking is acceptable. This part of the trait is
+	/// object safe but your closure must be boxed and you cannot have a return value.
+	//
+	fn spawn_blocking_dyn( &self, f: Box< dyn FnOnce()->R + Send > ) -> BlockingHandle<R>
+	{
+		self.inner().spawn_blocking_dyn(f)
+	}
+}
+
+
